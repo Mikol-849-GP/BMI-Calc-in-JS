c1ab075440f756eff66b4f5cbc81c0ea85ca6125
Mikol-849-GP <mikokondonikolau@interia.pl>
2025-10-02 17:18:53 +0200

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,1 +1,5 @@
-
+// Yes, I'm european
+var h = Number(prompt("Gimme your height in meters:"))
+var m = Number(prompt("Gimme your mass in kilograms:"))
+var BMI = (m / ( h * h))
+alert("Your BMI is ${BMI}")
